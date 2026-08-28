c6b4ae906f7e9f1e333dbe4d76ce2fa05f6a9ecf
Andres Benvenuto <andresbenve@gmail.com>
2026-08-28 15:17:05 +0000

    Apuntar a la implementacion publica del Apps Script

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
    Si algún día se vuelve a implementar el script, la URL no cambia:
    alcanza con "Administrar implementaciones → editar → Nueva versión". */
 window.GTM_CONFIG = {
-  api: 'https://script.google.com/macros/s/AKfycbzwqiCG9zcpJzh9WbEBHHjON519LusspiF_uG6wjA-oaN-wFF9lbogGChXdJtP-5pgy6A/exec'
+  api: 'https://script.google.com/macros/s/AKfycbzTmoNrA0Xboar_vM_kc_Ut98Y5MamWI6zGhijJadnQDbzneA1gq6iBBvwYiojv03Btzg/exec'
 };
